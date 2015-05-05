d967ce1be87a84faa40569c2ae093a938734345d
Lars <Post4Lars@t-online.de>
2015-05-05 21:36:35 +0200

    Installer

--- datenbank/installer.php
+++ datenbank/installer.php
@@ -1,1 +1,19 @@
 <?php
+
+$file_content = file('_DATABASE_TWITTER_.sql');
+$query = "";
+foreach($file_content as $sql_line)
+{
+	if(trim($sql_line) != "" == false)
+	{
+ 		$query = $sql_line;
+ 		if (substr(rtrim($query), -1) == ';')
+ 		{
+   			echo $query;
+   			$result = mysql_query($query)or exit(mysql_error(  ));
+   			$query = "";
+  		}
+ 	}
+}
+
+?>
