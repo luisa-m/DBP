c641b8490bf7213e68087c61db8fc115931cca29
Kevin <kevins.1994@web.de>
2015-05-05 22:25:46 +0200

    Funktion zum Verbinden ohne Datenbank hinzugefügt

--- hilf/db_helper.php
+++ hilf/db_helper.php
@@ -2,4 +2,7 @@
 function db_connect(){
 	return new PDO('mysql:host=localhost;dbname=hausarbeit_twitter', 'root', '');
 }
+function db_connect_wo_database(){
+	return new PDO('mysql:host=localhost', 'root', '');
+}
 ?>
