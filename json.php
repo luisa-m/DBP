543b9d48086c791c16308eba7e84d0ec3bad3160
Lars <Post4Lars@t-online.de>
2015-05-06 15:38:18 +0200

    json vervollständigen

--- json.php
+++ json.php
@@ -1,19 +1,21 @@
 <?php  
+require_once('fach/Kontext.php');
 $method = $_SERVER['REQUEST_METHOD'];
 $path = explode('/', trim($_SERVER['PATH_INFO'], '/'));
-
+$kontext = new Kontext();
+$benutzer = $kontext->getBenutzer();
 
 if($path[0] == "gefolgt" && $path[1] == "nachrichten")
 {
-	//require_once('fach/Benutzer.php');
-	//$nachrichten = Benutzer::XXXXX;
-	//echo json_encode($nachrichten);
+	require_once('fach/Benutzer.php');
+	$nachrichten = $benutzer->getGefolgteNachrichten();
+	echo json_encode($nachrichten);
 }
 elseif($path[0] == "gefolgt" && $path[1] == " benutzer")
 {
-	//require_once('fach/Benutzer.php');
-	//$follower = Benutzer::XXXXX;
-	//echo json_encode($follower);	
+	require_once('fach/Benutzer.php');
+	$follower = $benutzer->getGefolgte();
+	echo json_encode($follower);	
 }
 elseif($path[0] == "hashtag")
 {
