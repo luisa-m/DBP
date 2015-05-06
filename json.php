605c938d5f0a1ffa2c266f11a8f09315349bb8bf
Lars <Post4Lars@t-online.de>
2015-05-06 09:21:37 +0200

    json erweitert

--- json.php
+++ json.php
@@ -1,11 +1,24 @@
 <?php  
-
-require_once('fach/Nachricht.php');
-$hashtags = Nachricht::sucheNachHashtag("Hallo");
-echo json_encode($hashtags);
+$method = $_SERVER['REQUEST_METHOD'];
+$path = explode('/', trim($_SERVER['PATH_INFO'], '/'));
 
 
-hashtagSuche();
-
-
+if($path[0] == "gefolgt" && $path[1] == "nachrichten")
+{
+	//require_once('fach/Benutzer.php');
+	//$nachrichten = Benutzer::XXXXX;
+	//echo json_encode($nachrichten);
+}
+elseif($path[0] == "gefolgt" && $path[1] == " benutzer")
+{
+	//require_once('fach/Benutzer.php');
+	//$follower = Benutzer::XXXXX;
+	//echo json_encode($follower);	
+}
+elseif($path[0] == "hashtag")
+{
+	require_once('fach/Nachricht.php');
+	$hashtags = Nachricht::sucheNachHashtag($path[1]);
+	echo json_encode($hashtags);
+}
 ?>
