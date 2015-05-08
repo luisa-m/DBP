ea590e15bf4f1830255fc361fb503d6ff2af5373
Kevin <kevins.1994@web.de>
2015-05-08 08:31:52 +0200

    JSON-Client-Script für Timeline und gefolgte Benutzer

--- view/script.js
+++ view/script.js
@@ -12,11 +12,42 @@ function sucheHashtag(hashtag){
 	req.send();	
 }
 
+function zeigeTimeline(){
+	var req = new XMLHttpRequest();
+	req.open("GET", "../json.php/gefolgt/nachrichten", true);
+	req.onreadystatechange = function(e){
+		if (req.readyState == 4){
+			document.getElementById("ausgabe").innerHTML = formatiereNachrichten(JSON.parse(req.responseText));
+		}
+	};
+	req.send();		
+}
+
+function zeigeGefolgte(){
+	var req = new XMLHttpRequest();
+	req.open("GET", "../json.php/gefolgt/benutzer", true);
+	req.onreadystatechange = function(e){
+		if (req.readyState == 4){
+			document.getElementById("ausgabe").innerHTML = formatiereBenutzer(JSON.parse(req.responseText));
+		}
+	};
+	req.send();		
+}
+
 function formatiereNachrichten(nachrichten){
 	var res = "";
 	for (var i=0; i<nachrichten.length; i++){
 		nachricht = nachrichten[i];
 		res += '<div class="nachricht"><div class="nachrichtHeader"><span class="nachrichtName">'+nachricht["benutzer"]["vorname"]+" "+nachricht["benutzer"]["nachname"]+'</span><span class="nachrichtNickname">'+nachricht["benutzer"]["nickname"]+'</span><span class="nachrichtDatum">'+nachricht["datum"]["date"]+'</span></div><div class="nachrichtBody">'+nachricht["inhalt"]+'</div></div>';
+	}
+	return res;
+}
+
+function formatiereBenutzer(benutzers){
+	var res = "";
+	for (var i=0; i<benutzers.length; i++){
+		benutzer = benutzers[i];
+		res += '<div class="benutzer"><span class="benutzerName">'+benutzer["vorname"]+" "+benutzer["nachname"]+'</span><span class="benutzerNickname">'+benutzer["nickname"]+'</span>';
 	}
 	return res;
 }
